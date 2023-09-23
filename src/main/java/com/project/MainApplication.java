e778232b0102964bfdd45652741042fac871bd85
vincent-nicky <1935496654@qq.com>
2023-09-23 17:40:25 +0800

    update

--- src/main/java/com/project/MainApplication.java
+++ src/main/java/com/project/MainApplication.java
@@ -4,17 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * 主类（项目启动入口）
+ * - @EnableAspectJAutoProxy是一个Spring Framework注解，用于启用Spring AOP（Aspect-Oriented Programming）的支持。
+ * AOP是一种编程范式，允许开发人员通过切面（aspects）来模块化应用程序的横切关注点，例如日志记录、事务管理、安全性等。
+ * - @EnableAspectJAutoProxy注解告诉Spring容器启用AspectJ风格的AOP代理。
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class})
